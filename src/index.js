26f8b7fb07b1713019dc36b5e40fdbab1f640110
AndreiShal <31849804+AndreiShal@users.noreply.github.com>
2017-09-27 01:11:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
+  multiply('1', '1') // -> '1', because 1 * 1 === 1
+  multiply('2', '2') // -> '4', because 2 * 2 === 4
+  multiply('5', '10') // -> '50', because 5 * 10 === 50
   // your solution
+  
+  console.log(multiply(first, second) + ' because' + first + ' * ' + second + '===' + multiply(first, second));
 }
